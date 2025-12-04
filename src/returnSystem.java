6d20f3bf7df81ed2c48afc49f352fa9934fe30ec
parkjiwoo <bagjiw@naver.com>
2025-12-04 10:01:43 +0900

    array list 수정

--- src/returnSystem.java
+++ src/returnSystem.java
@@ -1,28 +1,27 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class returnSystem {
+
     Scanner sc = new Scanner(System.in);
 
-    public void returnBook(book[] books) {
+    public void returnBook(List<book> books) {
+
         System.out.print("반납할 도서 번호 입력: ");
         int id = sc.nextInt();
 
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).id == id) {
 
-        for (int i = 0; i < books.length; i++) {
-
-            if (books[i].id == id) {
-
-                if (books[i].status.equals("반납")) {
+                if (books.get(i).status.equals("반납")) {
                     System.out.println("이미 반납된 도서입니다.");
                 } else {
-                    books[i].status = "반납" ;
+                    books.get(i).status = "반납";
                     System.out.println("도서가 반납되었습니다.");
                 }
 
                 break;
             }
         }
-
-
-        }
     }
+}
--- src/searchSystem.java
+++ src/searchSystem.java
@@ -1,20 +1,20 @@
+import java.util.List;
 import java.util.Scanner;
 
-public class searchSystem{
+public class searchSystem {
+
     Scanner sc = new Scanner(System.in);
-    public void searchBook(book[] books) {
+
+    public void searchBook(List<book> books) {
 
         System.out.print("검색할 도서 번호 입력: ");
         int id = sc.nextInt();
 
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].id == id) {
-                books[i].printInfo();
-
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).id == id) {
+                books.get(i).printInfo();
                 break;
             }
         }
-
-
     }
 }
