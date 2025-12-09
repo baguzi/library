36a30c222f93ad202dc04ad0c41f69f02811d305
ptk <ppttkk9900@gmail.com>
2025-12-10 01:24:42 +0900

    addBooksystemTest 추가
    returnSystemTest, searchSystemTest 수정

--- test/returnSystemTest.java
+++ test/returnSystemTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,16 +14,14 @@ class returnSystemTest {
         String input = "475\n";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
 
-
-        book[] book1 = { new book(475, "미생물리학", "김병홍", "대출") };
-
+        List<book> books = new ArrayList<>();
+        books.add(new book(475, "미생물리학", "김병홍", "대출"));
 
         returnSystem system = new returnSystem();
-        system.returnBook(book1);
+        system.returnBook(books);
 
         assertTrue(output.toString().contains("도서가 반납되었습니다."));
     }
--- test/searchSystemTest.java
+++ test/searchSystemTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,12 +18,13 @@ class searchSystemTest {
         System.setOut(new PrintStream(output));
 
         book book1 = new book(475 , "미생물리학" , "김병홍","대출");
-        book[] books = { book1 };
+        List<book> books = new ArrayList<>();
+        books.add(book1);
 
         searchSystem system = new searchSystem();
         system.searchBook(books);
 
-        String correctOutput = "도서번호:475|도서이름:미생물리학|저자:김병홍|반납여부:대출";
+        String correctOutput = "도서번호:475 | 도서이름:미생물리학 | 저자:김병홍 | 반납여부:대출";
 
         assertTrue(output.toString().contains(correctOutput));
     }
